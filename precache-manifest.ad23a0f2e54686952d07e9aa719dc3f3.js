9ef19541f085f266c3ed570df2d1ccda7b27bb26
MilanC994 <cabo.milan@yahoo.com>
2020-02-07 03:32:33 +0100

    Updates

---
+++ precache-manifest.ad23a0f2e54686952d07e9aa719dc3f3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cca77b1cb2ec212af0ac41738044465",
+    "url": "/appRepository/index.html"
+  },
+  {
+    "revision": "98490a348178b1c9d21e",
+    "url": "/appRepository/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "1f94eaa8d9ec788950b5",
+    "url": "/appRepository/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "98490a348178b1c9d21e",
+    "url": "/appRepository/static/js/2.1953add9.chunk.js"
+  },
+  {
+    "revision": "0bdc4f0d8b3d84ae6a73bb9902b479ee",
+    "url": "/appRepository/static/js/2.1953add9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f94eaa8d9ec788950b5",
+    "url": "/appRepository/static/js/main.f3408d09.chunk.js"
+  },
+  {
+    "revision": "ee1ae0c4ab50a7339754",
+    "url": "/appRepository/static/js/runtime-main.38f3071a.js"
+  },
+  {
+    "revision": "e8bbf2fe49791d00dbf027343ccadd0c",
+    "url": "/appRepository/static/media/background.e8bbf2fe.jpeg"
+  }
+]);
