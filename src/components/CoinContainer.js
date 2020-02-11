c486547ad9b1dc9c46b1dcf6fec32d423c790586
MilanC994 <cabo.milan@yahoo.com>
2020-02-11 17:11:21 +0100

    changes

--- src/components/CoinContainer.js
+++ src/components/CoinContainer.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import '../App.css';
 
 function CoinContainer(props) {
     
@@ -8,11 +8,10 @@ function CoinContainer(props) {
         <tr>
         <div className="container">
            
-           <td> <span className ="badge m-2 badge-secondary"><h3>{props.value}</h3></span></td>
-           <td><span className ="badge m-2 badge-secondary"><h3>{props.count}</h3></span></td>
-            <td><button disabled={props.disable} onClick={()=>props.incrementProp(props.id)}   className=" btn btn-secondary m-2 btn-sm"><h3>+</h3></button></td>
-            <td><button disabled={props.disable}  onClick={()=>props.decrementProp(props.id)} className="btn btn-danger btn-default m-2"><h3>-</h3></button></td>
-            
+        <td> <span className ="myBadge badge m-2 badge-secondary"><h5>Value:{props.value}</h5></span></td>
+        <td><span className ="myBadge badge m-2 badge-secondary"><h5>Count:{props.count}</h5></span></td>
+        <td><button disabled={props.disable} onClick={()=>props.incrementProp(props.id)}   className="myBtn btn btn-secondary m-2"><h3>+</h3></button></td>
+        <td><button disabled={props.disable}  onClick={()=>props.decrementProp(props.id)} className="myBtn btn btn-danger  m-2"><h3>-</h3></button></td>
             
         </div>
         </tr>
